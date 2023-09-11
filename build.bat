2fcc0e27b2c6126eb69a8c849027a539bfb40471
mori1704 <1704mori@gmail.com>
2023-09-11 20:06:03 -0300

    add Entity.cpp and remove Food.cpp to build.bat

--- build.bat
+++ build.bat
@@ -14,7 +14,7 @@ set CXXFLAGS=-std=c++17 -I%VENDOR_DIR%/glfw/include -I%VENDOR_DIR%/imgui
 set LDFLAGS=-L%VENDOR_DIR%/glfw/lib -lglfw3 -lopengl32 -lgdi32 -limm32
 
 rem List of source files
-set SOURCES=%SRC_DIR%/main.cpp %SRC_DIR%/Game.cpp %SRC_DIR%/Snake.cpp %SRC_DIR%/Food.cpp %SRC_DIR%/Renderer.cpp %SRC_DIR%/common/Logger.cpp vendor/imgui/imgui.cpp vendor/imgui/imgui_draw.cpp vendor/imgui/imgui_impl_glfw.cpp vendor/imgui/imgui_impl_opengl3.cpp vendor/imgui/imgui_widgets.cpp vendor/imgui/imgui_tables.cpp
+set SOURCES=%SRC_DIR%/main.cpp %SRC_DIR%/Game.cpp %SRC_DIR%/Snake.cpp %SRC_DIR%/Entity.cpp %SRC_DIR%/Renderer.cpp %SRC_DIR%/common/Logger.cpp vendor/imgui/imgui.cpp vendor/imgui/imgui_draw.cpp vendor/imgui/imgui_impl_glfw.cpp vendor/imgui/imgui_impl_opengl3.cpp vendor/imgui/imgui_widgets.cpp vendor/imgui/imgui_tables.cpp
 
 rem Output executable file
 set OUTPUT=%BUILD_DIR%/snake.exe
